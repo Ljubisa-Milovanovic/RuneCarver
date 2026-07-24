df248af735eb9776baa647f8a12a200794cf336e
Luka Nikolić <142120455+Harascho1@users.noreply.github.com>
2026-07-24 03:03:52 +0200

    3 add settings scene (#9)
    * feat(settings): added prototype settings scene
    
    * feat(settings): added functional settings scene prototype

---
+++ Assets/Resources/Scripts/ButtonProps.cs
@@ -0,0 +1,69 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Resources.Scripts
+{
+    public class ButtonProps : MonoBehaviour
+    {
+        private enum ButtonAction
+        {
+            LoadScene,
+            Exit,
+            None
+        };
+
+        [SerializeField] private ButtonAction action;
+        [SerializeField] private string sceneName;
+
+        private Color _baseColor;
+        private Image _image;
+        private readonly Color _highlight = new(0.8549019607843137f, 0.8588235294117647f, 0.8666666666666667f, 0.5f);
+
+        private void Start()
+        {
+            _image = transform.GetComponent<Image>();
+            _baseColor = _image.color;
+            Debug.Log(_highlight);
+        }
+
+        private void OnMouseEnter()
+        {
+            _image.color = _highlight;
+        }
+
+        private void OnMouseExit()
+        {
+            _image.color = _baseColor;
+        }
+
+        private void OnMouseDown()
+        {
+            HandleClick();
+        }
+
+        private void HandleClick()
+        {
+            switch (action)
+            {
+                case ButtonAction.LoadScene:
+                    if (!string.IsNullOrEmpty(sceneName))
+                        SceneManager.LoadScene(sceneName);
+                    else
+                        Debug.LogWarning($"{gameObject.name}: scene isn't set");
+                    break;
+
+                case ButtonAction.Exit:
+                    Application.Quit();
+                    Debug.Log("Quit");
+                    break;
+
+                case ButtonAction.None:
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
