ebcdd6fced4fe0dae41bc3a2575af91caa2118ae
Luka Nikolić <142120455+Harascho1@users.noreply.github.com>
2026-07-26 22:18:55 +0200

    7 battle scene (#12)
    * part 1 battle scene
    
    * feature(hand Curve) - skoro pa zavrsen ima neki crazy ahh bug
    
    * fix(rebase): all directories start with a capital letter
    
    * fix(cards): cards are now showing in spline container.
    
    ---------
    
    Co-authored-by: Ljuba <ljubisanrt4022@gs.viser.edu.rs>

---
+++ Assets/Resources/Scripts/HandManager.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using DG.Tweening;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.Splines;
+
+namespace Resources.Scripts
+{
+    public class HandManager : MonoBehaviour
+    {
+        [SerializeField] private int maxHandSize;
+        [SerializeField] private GameObject cardPrefab;
+        [SerializeField] private SplineContainer splineContainer;
+        [SerializeField] private Transform spawnPoint;
+        [SerializeField] private GameObject cardHolder;
+
+        private readonly List<GameObject> _handCards = new();
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+                DebugReset();
+            if (Input.GetKeyDown(KeyCode.Space))
+                DrawCard();
+        }
+
+        private void DebugReset()
+        {
+            foreach (var card in _handCards)
+                Destroy(card);
+            _handCards.Clear();
+        }
+
+        private void DrawCard()
+        {
+            if (_handCards.Count >= maxHandSize) return;
+            var card = Instantiate(cardPrefab, spawnPoint.position, spawnPoint.rotation);
+            card.transform.SetParent(cardHolder.transform, false); //stavi true da vidis sta ce da se desi
+            _handCards.Add(card);
+            UpdateCardPosition();
+        }
+
+        // ReSharper disable Unity.PerformanceAnalysis
+        private void UpdateCardPosition()
+        {
+            if (_handCards.Count == 0)
+            {
+                Debug.Log("odigrane sve kartice, treba se pozove sledeca funcija za novu turu");
+                return;
+            }
+
+            // Making curvature
+            var cardSpacing = 1f / _handCards.Count;
+            var firstCardPosition = 0.5f - (_handCards.Count - 1) * cardSpacing / 2;
+
+            var i = 0;
+            foreach (var card in _handCards)
+            {
+                var position = firstCardPosition + i++ * cardSpacing; //position of each card
+                
+                splineContainer.Evaluate(position, out var pos, out var tangent, out var up);
+                Vector3 splinePosition   = pos;
+                Vector3 cardForwardDir   = -up;
+                Vector3 splineTangentDir = tangent;
+                
+                var cardUpDir = Vector3.Cross(cardForwardDir, splineTangentDir).normalized;
+                var rotation = Quaternion.LookRotation(cardForwardDir, cardUpDir);
+                card.transform.DOMove(splinePosition, 0.25f);
+                card.transform.DORotateQuaternion(rotation, 0.25f);
+            }
+        }
+    }
+}
